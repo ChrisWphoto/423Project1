15b8b7081c5df9e1d80c7cb40fec7ea0087d772f
ChrisWphoto <walter.chris@gmail.com>
2016-03-13 20:14:27 -0500

    working on second thread

--- 423EchoClient/Source.cpp
+++ 423EchoClient/Source.cpp
@@ -11,8 +11,16 @@ UDPEchoClient.cpp
 #include <string>
 #include <stdlib.h>
 #include <time.h>
-
+#include <process.h>
+#include <vector>
 using namespace std;
+
+//global which will be updated by secondary thread
+vector<string> recMsgs;
+
+/*
+	encryption function
+*/
 
 string crypt(string msg){
 	string eMsg = "";
@@ -25,6 +33,9 @@ string crypt(string msg){
 	return eMsg;
 }
 
+/*
+	Assumption: There will always be 2 semicolons in msg param.
+*/
 string deCrypt(char* msg){
 	if (msg[0] == '\0'){
 		return "empty reply";
@@ -48,6 +59,12 @@ string deCrypt(char* msg){
 	return rMsg;
 }
 
+//Global Server Vars
+WSADATA wsa;
+SOCKET s;
+struct sockaddr_in server;
+char *message, server_reply[500];
+int recv_size;
 
 int main(int argc, char *argv[]){
 	srand(time(NULL));
@@ -55,12 +72,7 @@ int main(int argc, char *argv[]){
 	int randMsgNum = (rand() % (RAND_MAX - min) + min);
 
 
-
-	WSADATA wsa;
-	SOCKET s;
-	struct sockaddr_in server;
-	char *message, server_reply[500];
-	int recv_size;
+	
 	cout << "Initialising Winsock...." << endl;
 	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)	{
 		printf("Failed. Error Code : %d", WSAGetLastError());		return 1;
@@ -74,31 +86,117 @@ int main(int argc, char *argv[]){
 	server.sin_addr.s_addr = inet_addr("204.76.188.23");
 	server.sin_family = AF_INET;
 	server.sin_port = htons(23456);
+	cout << "IP: 204.76.188.23 Port: 23456" << endl;
+	cout << "Starting MSG number: " << randMsgNum << endl;
 
 	//set SO_RESESADDR on a socket s to true	
 	int optval = 1;
 	setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (const char*)&optval, sizeof optval);
-	//Send some data	
-	string sMsg = to_string(randMsgNum) + ";1;" + crypt("chris");
-	message = const_cast<char*>( sMsg.c_str() );
-			
-	if (sendto(s, message, strlen(message), 0, (struct sockaddr*)&server, sizeof(server)) < 0)	{
-		cout << "Send Failed" << endl;
-		return 1;
-	}
-	cout << "Data Sent" << endl;
-	//Receive a reply from the server	
-	if ((recv_size = recvfrom(s, server_reply, 500, 0, NULL, NULL)) == SOCKET_ERROR)	{
-		cout << "recv failed" << endl;
-		return 1;
-	}
-	cout << "reply Received" << endl;
 
-	//Add \0 at the end of received string string before printing	
-	server_reply[recv_size] = '\0';	//puts(server_reply);	
-	string res = deCrypt(server_reply);
-	cout << res << endl;
-	cout << server_reply << endl;
+	/*
+		Start secondary thread for listening
+	*/
+	double temp = 10.0;
+	_beginthread(listen, 0, (void *)&temp);
+
+
+	
+	//Main Program Loop
+	string username;
+	int msgPlus = 0;
+	while (true) {
+		
+		//set username if not set
+		if (username == ""){
+			cout << "Welcome to Chat: Enter your username:";
+			cin >> username;
+
+
+			//Send user info to server
+			string sMsg = to_string(randMsgNum) + ";1;" + crypt(username);
+			message = const_cast<char*>(sMsg.c_str());
+
+			if (sendto(s, message, strlen(message), 0, (struct sockaddr*)&server, sizeof(server)) < 0)	{
+				cout << "Send Failed" << endl;
+				return 1;
+			}
+
+			//Receive a reply from the server	
+			if ((recv_size = recvfrom(s, server_reply, 500, 0, NULL, NULL)) == SOCKET_ERROR)	{
+				cout << "recv failed" << endl;
+				return 1;
+			}
+			server_reply[recv_size] = '\0';	//puts(server_reply);	
+			string res = deCrypt(server_reply);
+			cout << res << endl;
+		}
+		
+		//Display user menu
+		cout << "Enter q (for quit), s (send msg), or c (check for msgs): ";
+		char req;
+		cin >> req;
+		
+		//quit
+		if (req == 'q') break; 
+
+		//Send Dialog 
+		if (req == 's'){
+			cout << "Enter Buddy Name: ";
+			string buddy;
+			cin >> buddy;
+			cout << "Enter message here: ";
+			string userMsg;
+			cin >> userMsg;
+
+			//adding new line
+			buddy += '\n';
+
+			//increment msg
+			int randMsgPlus = ++randMsgNum;
+			//Construct Buddy Message
+			string sMsg = to_string(randMsgPlus) + ";2;" + crypt((username + '\n')) + crypt(buddy) + crypt(userMsg);
+			message = const_cast<char*>(sMsg.c_str());
+
+			//Send Message
+			if (sendto(s, message, strlen(message), 0, (struct sockaddr*)&server, sizeof(server)) < 0)	{
+				cout << "Send Failed" << endl;
+				return 1;
+			}
+			if ((recv_size = recvfrom(s, server_reply, 500, 0, NULL, NULL)) == SOCKET_ERROR)	{
+				cout << "recv failed" << endl;
+				return 1;
+			}
+			cout << "Msg Sent Successfully" << endl;
+		}
+
+
+		if (req == 'c'){
+			//Send some data	
+			string sMsg = to_string(randMsgNum++) + ";1;" + crypt(username);
+			message = const_cast<char*>(sMsg.c_str());
+
+			if (sendto(s, message, strlen(message), 0, (struct sockaddr*)&server, sizeof(server)) < 0)	{
+				cout << "Send Failed" << endl;
+				return 1;
+			}
+			cout << "Msg Sent" << endl;
+		}
+
+		//Receive a reply from the server	
+		if ((recv_size = recvfrom(s, server_reply, 500, 0, NULL, NULL)) == SOCKET_ERROR)	{
+			cout << "recv failed" << endl;
+			return 1;
+		}
+		
+
+		//Add \0 at the end of received string string before printing	
+		server_reply[recv_size] = '\0';	//puts(server_reply);	
+		string res = deCrypt(server_reply);
+		cout << res << endl;
+
+
+	}
+
 	closesocket(s);
 	WSACleanup();
 	system("pause"); 
@@ -107,72 +205,13 @@ int main(int argc, char *argv[]){
 
 
 
-
-
-
-/*
-
-#include <stdio.h>
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
-#include <winsock2.h>
-
-#include <ws2tcpip.h>
-#include <iostream>
-
-
-using namespace std;
-
-
-int main(int argc, char *argv[]){
-	WSADATA wsa;
-	SOCKET s;
-	struct sockaddr_in server;
-	char *message, server_reply[500];
-	int recv_size;
-	cout << "Initialising Winsock...." << endl;
-	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)	{
-		printf("Failed. Error Code : %d", WSAGetLastError());		return 1;
-	}
-	cout << "Initialised" << endl;
-
-	//Create a socket	
-	if ((s = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)	{
-		printf("Could not create socket : %d", WSAGetLastError());
-		return 1;
-	}
-	cout << "socket created" << endl;
-
-	server.sin_addr.s_addr = inet_addr("204.76.188.23");
-	server.sin_family = AF_INET;
-	server.sin_port = htons(23456);
-
-		int optval = 1;
-	setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (const char*)&optval, sizeof optval);
-
-	if (connect(s, (struct sockaddr *)&server, sizeof(server)) < 0)	{
-		cout << "connection error" << endl;
-		return 1;
-	}
-	cout << "connected" << endl;
-	//Send some data	
-	message = "22222;1;Chris";
-	if (send(s, message, strlen(message), 0) < 0)	{
-		cout << "Send Failed" << endl;
-		return 1;
-	}
-	cout << "Data Sent" << endl;
+void listen(void * temp){
+	double conversion = *(double *)temp;
 	//Receive a reply from the server	
-	if ((recv_size = recv(s, server_reply, 500, 0)) == SOCKET_ERROR)	{
+	if ((recv_size = recvfrom(s, server_reply, 500, 0, NULL, NULL)) == SOCKET_ERROR)	{
 		cout << "recv failed" << endl;
-		return 1;
 	}
-	cout << "reply Received" << endl;
-	//Add \0 at the end of received string 
-	server_reply[recv_size] = '\0';
 	cout << server_reply << endl;
-	closesocket(s);
-	WSACleanup();
-	return 0;
+
 }
 
-*/
